9fc6300c02bde1eda8ee34010d204e746e07d2e8
高坚果 <753610399@qq.com>
2019-04-18 09:34:16 +0800

    更新时间

--- src/recorder-core.js
+++ src/recorder-core.js
@@ -6,7 +6,7 @@ https://github.com/xiangyuecn/Recorder
 "use strict";
 
 //兼容环境
-window.RecorderLM="2019-01-06 21:38:00";
+window.RecorderLM="2019-4-18 09:31:44";
 var NOOP=function(){};
 //end 兼容环境 ****从以下开始copy源码，到wav、mp3前面为止*****
 
