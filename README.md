3dd1e20a3cd6efcb2c49e158f55f3c9240dd34fd
高坚果 <753610399@qq.com>
2020-01-14 17:45:48 +0800

    优化FrequencyHistogramView扩展增强0-10khz语音部分的显示

--- README.md
+++ README.md
@@ -612,7 +612,7 @@ set={
 
 
 ## `FrequencyHistogramView`扩展
-[frequency.histogram.view.js](https://github.com/xiangyuecn/Recorder/blob/master/src/extensions/frequency.histogram.view.js) + [lib.fft.js](https://github.com/xiangyuecn/Recorder/blob/master/src/extensions/lib.fft.js)，12kb大小源码，音频可视化频率直方图显示，具体样子参考演示地址页面。此扩展核心算法参考Java开源库[jmp123](https://sourceforge.net/projects/jmp123/files/)的代码编写的，`jmp123`版本`0.3`。
+[frequency.histogram.view.js](https://github.com/xiangyuecn/Recorder/blob/master/src/extensions/frequency.histogram.view.js) + [lib.fft.js](https://github.com/xiangyuecn/Recorder/blob/master/src/extensions/lib.fft.js)，12kb大小源码，音频可视化频率直方图显示，具体样子参考演示地址页面。此扩展核心算法参考Java开源库[jmp123](https://sourceforge.net/projects/jmp123/files/)的代码编写的，`jmp123`版本`0.3`；直方图特意优化主要显示0-10khz语音部分，其他高频显示区域较小，不适合用来展示音乐频谱。
 
 此扩展的使用方式和`WaveView`扩展完全相同，请参考上面的`WaveView`来使用；请注意：必须同时引入`lib.fft.js`才能正常工作。
 
@@ -622,41 +622,41 @@ set={
 构造函数，`set`参数为配置对象，默认配置值如下：
 ``` javascript
 set={
-	elem:"css selector" //自动显示到dom，并以此dom大小为显示大小
-		//或者配置显示大小，手动把frequencyObj.elem显示到别的地方
-	,width:0 //显示宽度
-	,height:0 //显示高度
-	
-	//以上配置二选一
-	
-	scale:2 //缩放系数，应为正整数，使用2(3? no!)倍宽高进行绘制，避免移动端绘制模糊
-	
-	,fps:20 //绘制帧率，不可过高
-	
-	,lineCount:15 //直方图柱子数量
-	,lineWidth:10 //柱子线条基础粗细
-	,minHeight:0 //柱子保留基础高度，position不为±1时应该保留点高度
-	,position:-1 //绘制位置，取值-1到1，-1为最底下，0为中间，1为最顶上，小数为百分比
-	
-	,stripeEnable:true //是否启用柱子顶上的峰值小横条，position不是-1时应当关闭，否则会很丑
-	,stripeHeight:5 //峰值小横条基础高度
-	,stripeMargin:8 //峰值小横条和柱子保持的基础距离
-	
-	,fallDuration:600 //柱子从最顶上下降到最底部最长时间ms
-	,stripeFallDuration:1200 //峰值小横条从最顶上下降到底部最长时间ms
-	
-	//柱子颜色配置：[位置，css颜色，...] 位置: 取值0.0-1.0之间
-	,linear:[0,"rgba(0,187,17,1)",0.5,"rgba(255,215,0,1)",1,"rgba(255,102,0,1)"]
-	//峰值小横条css颜色 #abc，留空为柱子的渐变颜色
-	,stripeColor:""
-	
-	,shadowBlur:0 //柱子阴影基础大小，设为0不显示阴影
-	,shadowColor:"#bbb" //柱子阴影颜色
-	,stripeShadowBlur:-1 //峰值小横条阴影基础大小，设为0不显示阴影，-1为柱子的大小
-	,stripeShadowColor:"" //峰值小横条阴影颜色，留空为柱子的阴影颜色
-	
-	//当发生绘制时会回调此方法，参数为当前绘制的频率数据和采样率，可实现多个直方图同时绘制，只消耗一个input输入和计算时间
-	,onDraw:function(frequencyData,sampleRate){}
+    elem:"css selector" //自动显示到dom，并以此dom大小为显示大小
+        //或者配置显示大小，手动把frequencyObj.elem显示到别的地方
+    ,width:0 //显示宽度
+    ,height:0 //显示高度
+    
+    //以上配置二选一
+    
+    scale:2 //缩放系数，应为正整数，使用2(3? no!)倍宽高进行绘制，避免移动端绘制模糊
+    
+    ,fps:20 //绘制帧率，不可过高
+    
+    ,lineCount:30 //直方图柱子数量，数量的多少对性能影响不大，密集运算集中在FFT算法中
+    ,lineWidth:6 //柱子线条基础粗细，当视图可以容下lineCount这么多时才会生效，否则自适应
+    ,minHeight:0 //柱子保留基础高度，position不为±1时应该保留点高度
+    ,position:-1 //绘制位置，取值-1到1，-1为最底下，0为中间，1为最顶上，小数为百分比
+    
+    ,stripeEnable:true //是否启用柱子顶上的峰值小横条，position不是-1时应当关闭，否则会很丑
+    ,stripeHeight:3 //峰值小横条基础高度
+    ,stripeMargin:6 //峰值小横条和柱子保持的基础距离
+    
+    ,fallDuration:1000 //柱子从最顶上下降到最底部最长时间ms
+    ,stripeFallDuration:3500 //峰值小横条从最顶上下降到底部最长时间ms
+    
+    //柱子颜色配置：[位置，css颜色，...] 位置: 取值0.0-1.0之间
+    ,linear:[0,"rgba(0,187,17,1)",0.5,"rgba(255,215,0,1)",1,"rgba(255,102,0,1)"]
+    //峰值小横条渐变颜色配置，取值格式和linear一致，留空为柱子的渐变颜色
+    ,stripeLinear:null
+    
+    ,shadowBlur:0 //柱子阴影基础大小，设为0不显示阴影
+    ,shadowColor:"#bbb" //柱子阴影颜色
+    ,stripeShadowBlur:-1 //峰值小横条阴影基础大小，设为0不显示阴影，-1为柱子的大小
+    ,stripeShadowColor:"" //峰值小横条阴影颜色，留空为柱子的阴影颜色
+    
+    //当发生绘制时会回调此方法，参数为当前绘制的频率数据和采样率，可实现多个直方图同时绘制，只消耗一个input输入和计算时间
+    ,onDraw:function(frequencyData,sampleRate){}
 }
 ```
 
--- src/extensions/frequency.histogram.view.js
+++ src/extensions/frequency.histogram.view.js
@@ -1,6 +1,6 @@
 /*
 录音 Recorder扩展，频率直方图显示
-使用本扩展需要引入lib.fft.js支持
+使用本扩展需要引入lib.fft.js支持，直方图特意优化主要显示0-10khz语音部分，其他高频显示区域较小，不适合用来展示音乐频谱
 
 https://github.com/xiangyuecn/Recorder
 
@@ -30,22 +30,22 @@ var fn=function(set){
 		
 		,fps:20 //绘制帧率，不可过高
 		
-		,lineCount:15 //直方图柱子数量
-		,lineWidth:10 //柱子线条基础粗细
+		,lineCount:30 //直方图柱子数量，数量的多少对性能影响不大，密集运算集中在FFT算法中
+		,lineWidth:6 //柱子线条基础粗细，当视图可以容下lineCount这么多时才会生效，否则自适应
 		,minHeight:0 //柱子保留基础高度，position不为±1时应该保留点高度
 		,position:-1 //绘制位置，取值-1到1，-1为最底下，0为中间，1为最顶上，小数为百分比
 		
 		,stripeEnable:true //是否启用柱子顶上的峰值小横条，position不是-1时应当关闭，否则会很丑
-		,stripeHeight:5 //峰值小横条基础高度
-		,stripeMargin:8 //峰值小横条和柱子保持的基础距离
+		,stripeHeight:3 //峰值小横条基础高度
+		,stripeMargin:6 //峰值小横条和柱子保持的基础距离
 		
-		,fallDuration:600 //柱子从最顶上下降到最底部最长时间ms
-		,stripeFallDuration:1200 //峰值小横条从最顶上下降到底部最长时间ms
+		,fallDuration:1000 //柱子从最顶上下降到最底部最长时间ms
+		,stripeFallDuration:3500 //峰值小横条从最顶上下降到底部最长时间ms
 		
 		//柱子颜色配置：[位置，css颜色，...] 位置: 取值0.0-1.0之间
 		,linear:[0,"rgba(0,187,17,1)",0.5,"rgba(255,215,0,1)",1,"rgba(255,102,0,1)"]
-		//峰值小横条css颜色 #abc，留空为柱子的渐变颜色
-		,stripeColor:""
+		//峰值小横条渐变颜色配置，取值格式和linear一致，留空为柱子的渐变颜色
+		,stripeLinear:null
 		
 		,shadowBlur:0 //柱子阴影基础大小，设为0不显示阴影
 		,shadowColor:"#bbb" //柱子阴影颜色
@@ -129,7 +129,7 @@ fn.prototype=FrequencyHistogramView.prototype={
 		
 		var now=Date.now();
 		var drawTime=This.drawTime||0;
-		if(now-This.inputTime>set.stripeFallDuration+500){
+		if(now-This.inputTime>set.stripeFallDuration*1.3){
 			//超时没有输入，顶部横条已全部落下，干掉定时器
 			clearInterval(This.timer);
 			This.timer=0;
@@ -166,22 +166,6 @@ fn.prototype=FrequencyHistogramView.prototype={
 		var bufferSize=This.fft.bufferSize;
 		
 		
-		//非线性划分频段 AudioGUI.java jmp123.gui.AudioGUI.setPlot方法
-		var fftIdxs=[];//每个柱子所在频段的索引区间
-		var maxFs=sampleRate >> 1;
-		var fsband = bufferSize >> 1;
-		if(maxFs > 20000){
-			var deltaFs = maxFs / fsband;
-			fsband -= (maxFs - 20000) / deltaFs;
-		};
-		for(var i =0; i<=lineCount; i++){
-			fftIdxs[i] = Math.floor(0.5 + Math.pow(fsband, i / lineCount));
-			if (i > 0 && fftIdxs[i] <= fftIdxs[i - 1]){
-				fftIdxs[i]=fftIdxs[i - 1]+1;
-			};
-		};
-		
-		
 		//计算高度位置
 		var position=set.position;
 		var posAbs=Math.abs(set.position);
@@ -199,15 +183,36 @@ fn.prototype=FrequencyHistogramView.prototype={
 		var speed=Math.ceil(heightY/(set.fallDuration/(1000/set.fps)));
 		var stripeSpeed=Math.ceil(heightY/(set.stripeFallDuration/(1000/set.fps)));
 		var stripeMargin=set.stripeMargin*scale;
+		
 		var Y0=1 << (Math.round(Math.log(bufferSize)/Math.log(2) + 3) << 1);
 		var logY0 = Math.log(Y0)/Math.log(10);
 		var dBmax=20*Math.log(0x7fff)/Math.log(10);
+		
+		var fftSize=bufferSize/2;
+		var fftSize10k=Math.floor(fftSize*10000/sampleRate);//10khz所在位置
+		var line80=Math.round(lineCount*0.8);//80%的柱子位置
+		var fftSizeStep1=fftSize10k/line80;
+		var fftSizeStep2=(fftSize-fftSize10k)/(lineCount-line80);
+		var fftIdx=0;
 		for(var i=0;i<lineCount;i++){
+			//不采用jmp123的非线性划分频段，录音语音并不适用于音乐的频率，应当弱化高频部分
+			//80%关注0-10khz主要人声部分 20%关注剩下的高频，这样不管什么采样率都能做到大部分频率显示一致。
+			var start=Math.ceil(fftIdx);
+			if(i<line80){
+				//10khz以下
+				fftIdx+=fftSizeStep1;
+			}else{
+				//10khz以上
+				fftIdx+=fftSizeStep2;
+			};
+			var end=Math.floor(fftIdx);
+			
+			
 			//参考AudioGUI.java .drawHistogram方法
 			
 			//查找当前频段的最大"幅值"
 			var maxAmp=0;
-			for (var j=fftIdxs[i],len=fftIdxs[i+1]; j<len; j++) {
+			for (var j=start; j<end; j++) {
 				maxAmp=Math.max(maxAmp,Math.abs(frequencyData[j]));
 			};
 			
@@ -222,7 +227,7 @@ fn.prototype=FrequencyHistogramView.prototype={
 			lastH[i]=h;
 			
 			var shi=stripesH[i]||0;
-			if(h>shi) {
+			if(h&&h+stripeMargin>shi) {
 				stripesH[i]=h+stripeMargin;
 			}else{
 				//使峰值小横条匀速度下落
@@ -236,14 +241,17 @@ fn.prototype=FrequencyHistogramView.prototype={
 		ctx.clearRect(0,0,width,height);
 		
 		var linear1=This.genLinear(ctx,set.linear,originY,originY-heightY);//上半部分的填充
+		var stripeLinear1=set.stripeLinear&&This.genLinear(ctx,set.stripeLinear,originY,originY-heightY)||linear1;//上半部分的峰值小横条填充
+		
 		var linear2=This.genLinear(ctx,set.linear,originY,originY+heightY);//下半部分的填充
+		var stripeLinear2=set.stripeLinear&&This.genLinear(ctx,set.stripeLinear,originY,originY+heightY)||linear2;//上半部分的峰值小横条填充
 		
 		//绘制柱子
 		ctx.shadowBlur=set.shadowBlur*scale;
 		ctx.shadowColor=set.shadowColor;
 		var lineWidth=set.lineWidth*scale;
 		if(lineWidth*lineCount+(lineCount+1)*scale>width){//放不下了，调小点
-			lineWidth=Math.floor((width-(lineCount+1)*scale)/lineCount);
+			lineWidth=Math.max(1*scale,Math.floor((width-(lineCount+1)*scale)/lineCount));
 		};
 		var spaceFloat=(width-lineCount*lineWidth)/(lineCount+1);//均匀间隔，首尾都留空
 		var minHeight=set.minHeight*scale;
@@ -282,7 +290,7 @@ fn.prototype=FrequencyHistogramView.prototype={
 				if(originY!=0){
 					y=originY-h-stripeHeight;
 					if(y<0){y=0;};
-					ctx.fillStyle=set.stripeColor||linear1;
+					ctx.fillStyle=stripeLinear1;
 					ctx.fillRect(x, y, lineWidth, stripeHeight);
 				};
 				//绘制下半部分
@@ -291,7 +299,7 @@ fn.prototype=FrequencyHistogramView.prototype={
 					if(y+stripeHeight>height){
 						y=height-stripeHeight;
 					};
-					ctx.fillStyle=set.stripeColor||linear2;
+					ctx.fillStyle=stripeLinear2;
 					ctx.fillRect(x, y, lineWidth, stripeHeight);
 				};
 				
